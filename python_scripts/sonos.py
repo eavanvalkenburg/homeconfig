f028007f353aba0d3b0fff9ad677e89a2fdb74ad
Eduard van Valkenburg <edvan@microsoft.com>
2019-04-02 18:04:59 +0200

    python script for sonos

---
+++ python_scripts/sonos.py
@@ -0,0 +1,44 @@
+
+"""       Requires python_script: to be enabled in you configuration       """
+
+
+""" Usage:                                                                 """
+"""                                                                        """
+""" automation:                                                            """
+"""   alias: Refresh John's birthday sensor                                """
+"""   trigger:                                                             """
+"""     platform: time                                                     """
+"""     at: '00:00:01'                                                     """
+"""   action:                                                              """
+"""     service: python_script.date_countdown                              """
+"""     data:                                                              """
+"""       action: ungroup                                                       """
+"""       speakers: 
+            - living room
+            - study                                                   """
+"""      source: tv                                   """
+
+dom = 'media_player'
+test = { "action": "join", "speakers": "living room, kitchen", "source": "tv" }
+sources = {'Line-in', 'TV'}
+actions = {'join', 'unjoin'}
+
+action = data.get('action')
+logger.info(action)
+speakers = data.get('speakers')
+logger.info(speakers)
+source = data.get('source')
+logger.info(source)
+
+if action in actions:
+    if action == 'join':
+        service = 'sonos_join' 
+        service_data = { "master": speakers[0], "entity_id": speakers[1:] }
+    else:
+        service = 'sonos_unjoin'
+        service_data = { "entity_id": speakers }
+    hass.async_call(dom, service, service_data )
+
+if source is sources:
+    speaker = 'media_player.study' if source == 'Line-in' else 'media_player.living_room'
+    hass.async_call(dom, 'select_source', { "entity_id": speaker, " source": source } )
